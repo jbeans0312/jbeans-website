89f93b6e454d0302576e6bcaff62ced4fd91812b
jbeans0312 <jbeans@udel.edu>
2023-01-17 12:30:18 -0500

    fixed rendering problems

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Resume } from "./pages/Resume";
 import "./App.css";
 
 function App(): JSX.Element {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="resume" element={<Resume />} />
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/resume" element={<Resume />} />
+        </Routes>
     );
 }
 
