5dd2aa5cecb46bb89b5c8584a4be2f00bb2db5b9
jbeans0312 <jbeans@udel.edu>
2023-02-05 11:16:59 -0500

    finished planner project section

--- src/components/PlannerProjectContent.tsx
+++ src/components/PlannerProjectContent.tsx
@@ -1,10 +1,38 @@
 import React from "react";
 import PlanStyle from "./style/PlannerProject.module.css";
+import { Text } from "@chakra-ui/react";
+import planner from "../images/plannerhomepage.png";
+import { ExternalLink } from "react-external-link";
 
 export function PlannerProjectContent(): JSX.Element {
+    const text1 = new String(
+        "My introduction to web development was through CISC275 at UD.\n\n"
+    );
+
+    const text2 = new String(
+        "Our group worked using an Agile system to complete the project through a series of sprints.\n\n"
+    );
+
+    const text3 = new String(
+        "Our site is a tool that UD students can use to create a four year plan or to plan individual semesters.\n\n"
+    );
+
     return (
-        <div className={PlanStyle.content}>
-            <h1>Bye</h1>
+        <div className={PlanStyle.contentwrapper}>
+            <h1 className={PlanStyle.h1}>UDCIS Student Planner</h1>
+            <div className={PlanStyle.content}>
+                <Text className={PlanStyle.basetext}>
+                    {text1}
+                    {text2}
+                    {text3}
+                </Text>
+                <div className={PlanStyle.imgwrapper}>
+                    <img src={planner} className={PlanStyle.contentimg}></img>
+                    <ExternalLink href="https://ud-cisc275-s22.github.io/cis-scheduler-team-14/">
+                        <span>Click to GitHub deployment (new tab)</span>
+                    </ExternalLink>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/PlannerProjectThumb.tsx
+++ src/components/PlannerProjectThumb.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import PlanStyle from "./style/PlannerProject.module.css";
+import thumb from "../images/plannerthumb.png";
 export function PlannerProjectThumb(): JSX.Element {
     return (
         <div className={PlanStyle.thumb}>
-            <h1>Hi</h1>
+            <div className={PlanStyle.imgborder}>
+                <img className={PlanStyle.img} src={thumb}></img>
+            </div>
         </div>
     );
 }
